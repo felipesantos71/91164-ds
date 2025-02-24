a4174f7f81423c25eb961514af1865f9d289f6cf
Felipe Santos <159580121+felipesantos71@users.noreply.github.com>
2025-02-24 13:56:31 +0000

    fripong

--- FuncionarioComEnum/SetorFuncionario.java
+++
@@ -1,6 +0,0 @@
-public enum SetorFuncionario {
-    FINANCEIRO,
-    RECURSOS_HUMANOS,
-    VENDAS,
-    MARKETING;
-}
--- enums/MainEnums.java
+++
@@ -1,7 +0,0 @@
-public class MainEnums {
-    public static void main(String[] args) {
-        Clientes clienteEnum = new Clientes("Maria", 42, Sexo.FEMININO);
-
-        System.out.println(clienteEnum.toString());
-    }
-}
